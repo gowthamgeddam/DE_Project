fb8c0c99bf5c33ebc606eab34f65826485db02a4
Geddam Gowtham <142402006@smail.iitpkd.ac.in>
2024-11-15 03:52:40 +0530

    GOD MODE updates, fixes #27

--- python_files/admin_tables.py
+++ python_files/admin_tables.py
@@ -366,10 +366,63 @@ def show_release_details_table():
 
 def show_god_mode():
     # Page to add all movie details at once
-    coln1, coln2, coln3 = st.columns([1, 1, 1])
-    with coln1:
-        st.markdown('---')
-    with coln2:
-        st.markdown('## Work in Progress')
-    with coln3:
-        st.markdown('---')
+    st.markdown("### Add all details of a movie:")
+    movie_id = st.number_input("Movie ID")
+    org_title = st.text_input("original title")
+    keywords = st.text_input("List of keywords (in braces'\{\}')", placeholder="{word1,word2,..}")
+    tagline = st.text_input("Tagline")
+    runtime = st.number_input("runtime (mins)")
+    homepage = st.text_input("Homepage URL", placeholder="https://iitpkd.ac.in/")
+    overview = st.text_input("overview")
+    imdb_id = st.text_input("IMDb ID", placeholder="tt970456")
+    popularity = st.number_input("Popularity")
+    vote_count = st.number_input("Vote Count")
+    vote_average = st.number_input("Vote Average")
+    directors_list = st.text_input("List of directors", placeholder="Chris Nolan, John Nolan, ...")
+    actors_list = st.text_input("List of actors", placeholder="John Riley, Harold Finch, ...")
+    genres_list = st.text_input("List of genre", placeholder="Adventure, Thriller, ...")
+    producers_list = st.text_input("List of production companies", placeholder="BioScope Films Ltd., BadRobot, ...")
+    budget = st.number_input("Budget")
+    revenue = st.number_input("Revenue")
+    budget_adj = st.number_input("Budget_Adj")
+    revenue_adj = st.number_input("Revenue_Adj")
+    release_date = st.date_input("Release Date", format='YYYY-MM-DD')
+    release_year = st.number_input("Release Year")
+
+    if st.button("Add all details"):
+        query_list = []
+        # Insert into movie table
+        query_list.append(f"INSERT INTO movie (id, original_title) VALUES ({movie_id}, '{org_title}')")
+        # Add metadata 
+        query_list.append(f"INSERT INTO metadata (id, keywords, tagline, runtime, homepage, overview) VALUES ({movie_id}, '{keywords}', '{tagline}', {runtime}, '{homepage}', '{overview}' )")
+        # Add imdb details
+        query_list.append(f"INSERT INTO imdb_details (id, imdb_id, popularity, vote_count, vote_average) VALUES ({movie_id}, '{imdb_id}', {popularity}, {vote_count}, {vote_average})")
+        # Add each director
+        director_names = directors_list.split(',')
+        for name in director_names:
+            query_list.append(f"INSERT INTO directed (id, director_name) VALUES ({movie_id}, '{name}')")
+        # Add each actor
+        actor_names = actors_list.split(',')
+        for name in actor_names:
+            query_list.append(f"INSERT INTO movie_cast (id, actor_name) VALUES ({movie_id}, '{name}')")
+        # Add each genre
+        genres = genres_list.split(',')
+        for genre in genres:
+            query_list.append(f"INSERT INTO movie_genre (id, genre) VALUES ({movie_id}, '{genre}')")
+        # Add each producer
+        producers = producers_list.split(',')
+        for producer in producers:
+            query_list.append(f"INSERT INTO production (id, production_company) VALUES ({movie_id}, '{producer}')")
+        # Add finances
+        query_list.append(f"INSERT INTO finances (id, budget, revenue, budget_adj, revenue_adj) VALUES ({movie_id}, {budget}, {revenue}, {budget_adj}, {revenue_adj})")
+        # Add release details
+        query_list.append(f"INSERT INTO release_details (id, release_date, release_year) VALUES ({movie_id}, '{release_date}', {release_year})")
+
+        try:
+            for each_query in query_list:
+                query_return = run_query(each_query)
+                if query_return!="Done":
+                    raise Exception    
+            st.success(f"All details added successfully! {query_return}")
+        except:
+            st.warning("Oops! Error inserting movie details, check your inputs or contact admin")
